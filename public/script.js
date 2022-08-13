7ec4d287dff38032b497f548cfdb706ca7bb7bb2
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2022-08-13 17:13:02 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,40 +1,53 @@
 const socket = io("/");
 
-const user = prompt("Enter your Name");
-
 var peer = new Peer(undefined, {
-    path:  "/peerjs",
+    path: "/peerjs",
     host: "/",
-    port: "443"
+    port: "443",
+});
+
+const user = prompt('Enter Your Name')
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+let myStream;
+
+navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true
 })
 
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        let html = `
+            <div class="user-container">
+                ${video.outerHTML}
+            </div>
+        `
+        $("#users").append(html)
+    });
+};
+
 $(function () {
-    $("#send").click(function () {
-        if ($("#chat_message").val().length !== 0) {
-            console.log(user);
-            socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
-        }
-    })
-    $("#chat_message").keydown(function (e) {
-        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
         }
     })
 })
 
 peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id, user);
-});
-
-socket.on("createMessage", (message, userName) => {
-    $(".messages").append(`
-        <div class="message">
-            <b><i class="far fa-user-circle"></i> <span> ${
-                userName === user ? "me" : userName
-            }</span> </b>
-            <span>${message}</span>
-        </div>
-    `)
+    socket.emit("join-room", ROOM_ID, id);
 });
